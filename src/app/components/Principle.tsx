bbeeb4fb0ea39594904f26de5e59b42ab370a604
Chris Pitts <cp25393@gmail.com>
2024-03-18 09:24:34 -0500

    some styling on principle component

--- src/app/components/Principle.tsx
+++ src/app/components/Principle.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React, { useState } from 'react';
 import Guideline from './Guideline';
 import { getPrincipleAudits } from '../api/db';
 
@@ -20,25 +20,35 @@ export default async function Principle({
 
 	const ratio = audits.length > 0 ? passedAudits / audits.length : 0;
 
+	// const [expanded, setExpanded] = useState(false);
+
 	let circleColor = ratio === 1 ? 'green' : ratio > 0.6 ? 'yellow' : 'red';
 
 	return (
 		<>
-			<h2 className='text-lg font-extrabold'>
-				Principle {principleNumber}: {principle.title}
-			</h2>
-			<p>
-				Passed:{' '}
-				<span
-					className='audit-circle'
-					style={{
-						'--data-progress': ratio,
-					'--data-color': circleColor} as React.CSSProperties}
-				>
-					{passedAudits}/{audits.length}
-				</span>
-			</p>
-			<p>{principle.description}</p>
+			<div className='dropdown-container'>
+				<div className='container-top'>
+					<h2 className='text-lg font-extrabold'>
+						Principle {principleNumber}: {principle.title}
+					</h2>
+					<p>
+						Passed:
+						<span
+							className='audit-circle m-4'
+							style={
+								{
+									'--data-progress': ratio,
+									'--data-color': circleColor,
+								} as React.CSSProperties
+							}
+						>
+							{passedAudits}/{audits.length}
+						</span>
+					</p>
+				</div>
+
+				<p>{principle.description}</p>
+			</div>
 
 			{Object.entries(principle.guidelines).map(
 				([key, value]: [key: string, value: any]) => {
